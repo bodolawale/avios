5b507d773595b9241727a62fe8f0ca519284987f
Isaac Olawale <bodolawale@gmail.com>
2021-04-29 10:15:52 +0100

    added cart schema

---
+++ models/cart.js
@@ -0,0 +1,19 @@
+const Sequelize = require("sequelize");
+const sequelize = require("../DB/sequelize");
+const Product = require("./product");
+
+// Define the Cart Schema
+const Cart = sequelize.define(
+	"cart",
+	{
+		//normally the user info would be here
+	},
+	{ timestamps: true, createdAt: "date_uploaded", updatedAt: "date_updated" }
+);
+
+Cart.sync({ alter: true });
+
+Cart.belongsToMany(Product, { through: "ProductCarts" });
+Product.belongsToMany(Cart, { through: "ProductCarts" });
+
+module.exports = Cart;
